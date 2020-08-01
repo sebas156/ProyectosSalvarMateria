9ca2849d3972cc60fe8a042ae37228cdf68a3f1d
LUCIFER <sebocruz@hotmail.com>
2020-08-01 00:09:50 -0500

    Se añadieron mas comentarios

--- PROYECTO_FINAL/BioHazard/game.h
+++ PROYECTO_FINAL/BioHazard/game.h
@@ -28,11 +28,11 @@ public:
     game(int*,int,string,QWidget *parent = 0);
     ~game();
     void SetNivelOrda(int,int);
-    QGraphicsScene * scene;
-    player1 * player;
+    QGraphicsScene * scene; // Variable para la escena.
+    player1 * player;       // Jugador.
     QGraphicsView * view;
     QMediaPlayer * music;
-    void SetZombiesPorOrda();
+    void SetZombiesPorOrda();   // Establece los zombies que se despliegan cada TiempoEntreOrdas segundos.
     void EstablecerMuros();
     bool PisoConFriccion();
     bool EstablecerPisoQuitaVida();
@@ -59,31 +59,31 @@ public:
     ganar * InterfazGanar;
 
 public slots:
-    void PausarTodoJuego();
-    void LiberarOrdasZombies();
-    void ActualizarCamporVectorial();
-    void ActualizarZombies();
-    void follow_char();
-    void PerdisteElJuego();
-    void VerificarSiYaPasadeNivel();
-    void ContinuarJugando();
+    void PausarTodoJuego();      // Slot que al ejecutarse para todos los timers.
+    void LiberarOrdasZombies();  // Libera una cierta cantidad de zombies cada TiempoEntreOrdas segundos.
+    void ActualizarCamporVectorial();  //
+    void ActualizarZombies();         // Actualiza la posicion de los zombies.
+    void follow_char();              //Santiagoooooo.... Este se elimina?
+    void PerdisteElJuego();          // Slot que detiene todos los timers y muestra la interfaz de perder.
+    void VerificarSiYaPasadeNivel(); // Verifica si pasa de nivel o en su defecto gana el juego
+    void ContinuarJugando();        // Inicia todos los timers.
 
 private:
     QTimer * OrdasZombies;
     QTimer * timer;
     QTimer * VerificarSiPasaNivel;
-    int nivel;
-    int Orda;
-    int TiempoEntreOrdas;
+    int nivel;                      //Nivel actual del juador.
+    int Orda;                       // Orda actual del jugador.
+    int TiempoEntreOrdas;           // Tiempo que hay entre en despliegue de zombies y otro.
     int modo;
-    int *NivelRetornar;
-    QString NickNameInicioSesion;
+    int *NivelRetornar;             // NivelRetornar recibe la direccion de memoria de la variable en la que se guardó el nivel que se selecciono.
+    QString NickNameInicioSesion;   // Almacena el nombre del usuario que ha iniciado sesion.
     QList <obstaculo *> Paredes;
-    nodo ArregloMatrizAbstracta[24][40];
-    int NumeroZombies;
-    queue <nodo *> frontera;
-    int NIX,NIY;
-    float t=0.1;
+    nodo ArregloMatrizAbstracta[24][40];  // Matriz de todos los nodos que cubren el mapa.
+    int NumeroZombies;                    // Es el numero de zombies que en total van a aparecer en cada una de las 3 oleadas.
+    queue <nodo *> frontera;              // Al amacena los nodos en los que el algoritmo de creacion del mapa de calor se va parando.
+    int NIX,NIY;                          // Guarda la posision en la matriz de los nodos iniciales.
+    float t=0.1;                          // Variable de tiempo para las ecuaciones de movimientos de los zombies.
 };
 
 #endif // GAME_H
